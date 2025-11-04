278d70213539a407fd9c00119cb739eb1b58423b
AeYunDian <zhanghaoyu19281@outlook.com>
2025-11-04 21:34:21 +0800

    Update m.js

--- database/m.js
+++ database/m.js
@@ -1,6 +1,6 @@
 var link_data = 
 {
 "uid": 1002,
-"to": "https://weixin76062.kk-rays.com/W66062/C1000026830/T1761747743325/content/pdf/answer/27488399?answerResourceId=84151&appId=2384667&timestamp=1761747775002",
+"to": "https://weixin76062.kk-rays.com/W66062/C1000026830/T1761747743325/content/pdf/answer/27488399?answerResourceId=84151&appId=2384667",
 "AR": [ "android_phone", "iPhone", "android_pad", "Windows_IE", "Windows_Chrome", "macOS", "iPad", "Symbian"]
 }
