455e77796f93af5308b895ee202709da1b4e9061
AeYunDian <zhanghaoyu19281@outlook.com>
2025-11-04 21:34:09 +0800

    Create c.js

---
+++ database/c.js
@@ -0,0 +1,6 @@
+var link_data = 
+{
+"uid": 1003,
+"to": "https://weixin23.5rs.me/W23/C1000026830/T1760348966258/content/pdf/answer/28587344?answerResourceId=102227&bookId=12683211&sceneId=4387299&adviserId=12829&appId=2687177",
+"AR": [ "android_phone", "iPhone", "android_pad", "Windows_IE", "Windows_Chrome", "macOS", "iPad", "Symbian"]
+}
