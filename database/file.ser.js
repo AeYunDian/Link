fcf2522cf668cc977cce58888df78eb04d927b75
AeYunDian <zhanghaoyu19281@outlook.com>
2025-11-04 21:36:21 +0800

    Create file.ser.js

---
+++ database/file.ser.js
@@ -0,0 +1,6 @@
+var link_data = 
+{
+"uid": 1006,
+"to": "http://120.55.244.147:5244",
+"AR": [ "android_phone", "iPhone", "android_pad", "Windows_IE", "Windows_Chrome", "macOS", "iPad", "Symbian"]
+}
